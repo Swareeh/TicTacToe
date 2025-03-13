143657700dd1bf7d9aac3b6a8022277ff68cd0d0
Swareeh <77015406+Swareeh@users.noreply.github.com>
2025-03-13 13:39:15 +0400

    Update script.js

--- script.js
+++ script.js
@@ -8,63 +8,198 @@ const box7 = document.getElementById('r3c1')
 const box8 = document.getElementById('r3c2')
 const box9 = document.getElementById('r3c3')
 
-var symbol = 'X'
-var gameOver = false
 
-
-function EventListeners(symbol) {
+function PlayerX() {
+    var turnOver = false
 
     box1.addEventListener('click', function () {
+        if (turnOver)return
         if (box1.innerHTML == 'X' || box1.innerHTML == 'O') {
             console.log('Slot already filled!')
         } else {
-            box1.innerHTML = symbol
+            box1.innerHTML = 'X'
+            turnOver = true
         }
     })
 
+
     box2.addEventListener('click', function () {
+        if (turnOver)return
         if (box2.innerHTML == 'X' || box2.innerHTML == 'O') {
             console.log('Slot already filled!')
 
         } else {
-            box2.innerHTML = symbol
+            box2.innerHTML = 'X'
+            turnOver = true
         }
     })
 
     box3.addEventListener('click', function () {
+        if (turnOver)return
         if (box3.innerHTML == 'X' || box3.innerHTML == 'O') {
             console.log('Slot already filled!')
         } else {
-            box3.innerHTML = symbol
+            box3.innerHTML = 'X'
+            turnOver = true
+        }
+    })
+    
+    box4.addEventListener('click', function () {
+        if (turnOver)return
+        if (box4.innerHTML == 'X' || box4.innerHTML == 'O') {
+            console.log('Slot already filled!')
+        } else {
+            box4.innerHTML = 'X'
+            turnOver = true
+        }
+    })
+
+    
+    box5.addEventListener('click', function () {
+        if (turnOver)return
+        if (box5.innerHTML == 'X' || box5.innerHTML == 'O') {
+            console.log('Slot already filled!')
+        } else {
+            box5.innerHTML = 'X'
+            turnOver = true
+        }
+    })
+    
+    box6.addEventListener('click', function () {
+        if (turnOver)return
+        if (box6.innerHTML == 'X' || box6.innerHTML == 'O') {
+            console.log('Slot already filled!')
+        } else {
+            box6.innerHTML = 'X'
+            turnOver = true
+        }
+    })
+    
+    box7.addEventListener('click', function () {
+        if (turnOver)return
+        if (box7.innerHTML == 'X' || box7.innerHTML == 'O') {
+            console.log('Slot already filled!')
+        } else {
+            box7.innerHTML = 'X'
+            turnOver = true
+        }
+    })
+    
+    box8.addEventListener('click', function () {
+        if (turnOver)return
+        if (box8.innerHTML == 'X' || box8.innerHTML == 'O') {
+            console.log('Slot already filled!')
+        } else {
+            box8.innerHTML = 'X'
+            turnOver = true
+        }
+    })
+    
+    box9.addEventListener('click', function () {
+        if (turnOver)return
+        if (box9.innerHTML == 'X' || box9.innerHTML == 'O') {
+            console.log('Slot already filled!')
+        } else {
+            box9.innerHTML = 'X'
+            turnOver = true
+        }
+    })    
+}
+
+function PlayerO() {
+    var turnOver = false
+
+    box1.addEventListener('click', function () {
+        if (turnOver)return
+        if (box1.innerHTML == 'X' || box1.innerHTML == 'O') {
+            console.log('Slot already filled!')
+        } else {
+            box1.innerHTML = 'O'
+            turnOver = true
         }
     })
 
 
-}
+    box2.addEventListener('click', function () {
+        if (turnOver)return
+        if (box2.innerHTML == 'X' || box2.innerHTML == 'O') {
+            console.log('Slot already filled!')
 
-function PlayerInputX() {
-    var symbol = 'X'
-    EventListeners(symbol)
-}
-
-function PlayerInputO() {
-    var symbol = 'O'
-    EventListeners(symbol)
-
-}
-
-function Ref() {
-    for (let i in ['X','O']) {
-        if (box1.innerHTML == box2.innerHTML  == box3.innerHTML == i) {
-            alert('Player',i,'Wins!')
+        } else {
+            box2.innerHTML = 'O'
+            turnOver = true
         }
-    }
+    })
+
+    box3.addEventListener('click', function () {
+        if (turnOver)return
+        if (box3.innerHTML == 'X' || box3.innerHTML == 'O') {
+            console.log('Slot already filled!')
+        } else {
+            box3.innerHTML = 'O'
+            turnOver = true
+        }
+    })
+    
+    box4.addEventListener('click', function () {
+        if (turnOver)return
+        if (box4.innerHTML == 'X' || box4.innerHTML == 'O') {
+            console.log('Slot already filled!')
+        } else {
+            box4.innerHTML = 'O'
+            turnOver = true
+        }
+    })
+
+    
+    box5.addEventListener('click', function () {
+        if (turnOver)return
+        if (box5.innerHTML == 'X' || box5.innerHTML == 'O') {
+            console.log('Slot already filled!')
+        } else {
+            box5.innerHTML = 'O'
+            turnOver = true
+        }
+    })
+    
+    box6.addEventListener('click', function () {
+        if (turnOver)return
+        if (box6.innerHTML == 'X' || box6.innerHTML == 'O') {
+            console.log('Slot already filled!')
+        } else {
+            box6.innerHTML = 'O'
+            turnOver = true
+        }
+    })
+    
+    box7.addEventListener('click', function () {
+        if (turnOver)return
+        if (box7.innerHTML == 'X' || box7.innerHTML == 'O') {
+            console.log('Slot already filled!')
+        } else {
+            box7.innerHTML = 'O'
+            turnOver = true
+        }
+    })
+    
+    box8.addEventListener('click', function () {
+        if (turnOver)return
+        if (box8.innerHTML == 'X' || box8.innerHTML == 'O') {
+            console.log('Slot already filled!')
+        } else {
+            box8.innerHTML = 'O'
+            turnOver = true
+        }
+    })
+    
+    box9.addEventListener('click', function () {
+        if (turnOver)return
+        if (box9.innerHTML == 'X' || box9.innerHTML == 'O') {
+            console.log('Slot already filled!')
+        } else {
+            box9.innerHTML = 'O'
+            turnOver = true
+        }
+    })    
 }
 
-
-for (let i = 0; i<=3;i++) {
-    PlayerInputX()
-    PlayerInputO()
-}
-
-// TODO: Need to Entirely rebuild the logic
