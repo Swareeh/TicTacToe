cbc607fa77fe28987167afc15599f8e26666a144
Swareeh <77015406+Swareeh@users.noreply.github.com>
2025-04-12 10:31:42 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// const box1 = document.getElementById('box1')
-// const box2 = document.getElementById('box2')
-// const box3 = document.getElementById('box3')
-// const box4 = document.getElementById('box4')
-// const box5 = document.getElementById('box5')
-// const box6 = document.getElementById('box6')
-// const box7 = document.getElementById('box7')
-// const box8 = document.getElementById('box8')
-// const box9 = document.getElementById('box9')
+const box1 = document.getElementById('box1')
+const box2 = document.getElementById('box2')
+const box3 = document.getElementById('box3')
+const box4 = document.getElementById('box4')
+const box5 = document.getElementById('box5')
+const box6 = document.getElementById('box6')
+const box7 = document.getElementById('box7')
+const box8 = document.getElementById('box8')
+const box9 = document.getElementById('box9')
 
 // let gameOver = false
 let PlayerTurn = 'X'
@@ -29,26 +29,52 @@ function Play(event) {
         }
     }
 
+    const result = Ref()
 
-    if (Ref() == 'Game Over') {
+    if (result == 'Game Over') {
         alert('Game is over! Draw')
         return
-    } else if (Ref() == `${document.getElementById('box1').innerHTML} Wins`) {
-        alert(`Game is over! ${document.getElementById('box1').innerHTML} Wins`)
+    }
+    else if (result == `X Wins`) {
+        alert(`Game is over! X Wins`)
+    } else if (result == `O Wins`) {
+        alert(`Game is over! O Wins`)
     }
 
 }
 
 function Ref() {
 
-    if (document.getElementById('box1').innerHTML == document.getElementById('box2').innerHTML == document.getElementById('box3').innerHTML in ['X','O']) {
-        return `${document.getElementById('box1').innerHTML} Wins`
+    //Checking Horizontally 
+    if (['X', 'O'].includes(box1.innerHTML) && box1.innerHTML == box2.innerHTML && box2.innerHTML == box3.innerHTML) {
+        return `${box1.innerHTML} Wins`
+
+    } else if (['X', 'O'].includes(box4.innerHTML) && box4.innerHTML == box5.innerHTML && box5.innerHTML == box6.innerHTML) {
+        return `${box4.innerHTML} Wins`
+
+    } else if (['X', 'O'].includes(box7.innerHTML) && box7.innerHTML == box8.innerHTML && box8.innerHTML == box9.innerHTML) {
+        return `${box7.innerHTML} Wins`
     }
 
+    //Checking Verticlly
+    else if (['X', 'O'].includes(box1.innerHTML) && box1.innerHTML == box4.innerHTML && box4.innerHTML == box7.innerHTML) {
+        return `${box1.innerHTML} Wins`
 
+    } else if (['X', 'O'].includes(box2.innerHTML) && box2.innerHTML == box5.innerHTML && box5.innerHTML == box8.innerHTML) {
+        return `${box2.innerHTML} Wins`
 
+    } else if (['X', 'O'].includes(box3.innerHTML) && box3.innerHTML == box6.innerHTML && box6.innerHTML == box9.innerHTML) {
+        return `${box3.innerHTML} Wins`
 
+        // Checking Diagonally
+    } else if (['X', 'O'].includes(box1.innerHTML) && box1.innerHTML == box5.innerHTML && box5.innerHTML == box9.innerHTML) {
+        return `${box1.innerHTML} Wins`
 
+    } else if (['X', 'O'].includes(box3.innerHTML) && box3.innerHTML == box5.innerHTML && box5.innerHTML == box7.innerHTML) {
+        return `${box3.innerHTML} Wins`
+    }
+
+    //Checking if there are any empty boxes left 
     let emptyspace = false
 
     for (let i = 1; i <= 9; i++) {
