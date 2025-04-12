84c129f2887f7105853e76f173e1c86cdb95e89a
Swareeh <77015406+Swareeh@users.noreply.github.com>
2025-04-12 23:56:22 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
 const play_btn = document.getElementById('btn_play')
 const about_btn = document.getElementById('btn_about')
-
-// play_btn.addEventListener("click", () => {
-//     window.location.replace('./Multiplayer/multiplayer.html')
-
-// })
-
-// about_btn.addEventListener("click", () => {
-//     window.location.replace('https://github.com/Swareeh/TicTacToe')
-
-// })
